518b12ffb92e01f56ea49b60e4889d5d415b3c6e
Dimitri Belopopsky <dimitri@belopopsky.com>
2017-10-28 18:26:37 +0200

    Animations: Resting and Moving sprites for main character

--- Player.gd
+++ Player.gd
@@ -4,10 +4,10 @@ extends KinematicBody2D
 var speed = Vector2()
 var velocity = Vector2()
 
-var SPEED = 500
-var MAX_SPEED = 1000
+var SPEED = 75
+var MAX_SPEED = 800
 
-var JUMP_FORCE = 800
+var JUMP_FORCE = 5000
 var GRAVITY = 100
 
 var direction = 0
@@ -22,17 +22,23 @@ func _input():
  
 func _process(delta):
 	if (Input.is_action_pressed("move_left")):
+		get_node("Sprite").set_flip_h(true)
+		get_node("Sprite").set_animation("Moving")
 		velocity.x = -SPEED
 		direction = -1
 	elif (Input.is_action_pressed("move_right")):
+		get_node("Sprite").set_flip_h(false)
+		get_node("Sprite").set_animation("Moving")
 		velocity.x = SPEED
 		direction = 1
 	else:
+		get_node("Sprite").set_animation("Resting")
 		velocity.x = 0
 		direction = 0
 		
 	if (Input.is_action_just_pressed("jump") && is_on_floor()):
-		velocity.y = -JUMP_FORCE
+		velocity.y -= JUMP_FORCE
+		velocity.x += 200 * direction
 	else:
 		velocity.y += GRAVITY
 	
@@ -40,5 +46,7 @@ func _process(delta):
 	velocity.y = clamp(velocity.y, -MAX_SPEED, MAX_SPEED)
 	
 	move_and_slide(velocity, Vector2(0, -1))
+	
+	previous_direction = direction
 	
 	
